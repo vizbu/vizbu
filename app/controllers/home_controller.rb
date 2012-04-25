4442ea5c1a05858decab50590468880bbc5d7bda
Amit Ambardekar <amitamb@gmail.com>
2012-04-25 21:41:11 +0530

    Added filtering support for view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,19 +1,13 @@
 class HomeController < ApplicationController
+
   def index
   end
 
   def search
     @q = params[:q]
-    @q = params[:q]
-    @q = params[:q]
-    @q = params[:q]
-    
     client = YouTubeIt::Client.new(:dev_key => "AI39si5-1s6CVSSGdBqlMnzN9v_OMBufAMEW-0H4Ke1UG5laQpDCWyWJU5WJlpVHPXSTHyBDHEoFsbBdLfwgHBs7Aic3tjHR0Q")
-
     @result = client.videos_by(:query => @q, :page => params[:page] || 1, :per_page => 20)
-
-    
-
+    @view = params[:view] || "list"
   end
 
   def about
