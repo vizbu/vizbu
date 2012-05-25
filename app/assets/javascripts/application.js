91f97efc64c8e73504427e7d786cec8ea064c572
Amit Ambardekar <amitamb@gmail.com>
2012-05-25 23:16:09 +0530

    Removed comments

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,9 @@
 function checkOverflow(el)
 {
    var curOverflow = el.style.overflow;
-   //if ( !curOverflow || curOverflow === "visible" )
-   //   el.style.overflow = "hidden";
 
    var isOverflowing = el.clientWidth < el.scrollWidth 
       || el.clientHeight < el.scrollHeight;
-
-   //el.style.overflow = curOverflow;
 
    return isOverflowing;
 }
