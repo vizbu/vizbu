6c66056c752e7e43eecef5f95821a96e9a42fe11
Phlip <phlip2005@gmail.com>
2012-05-26 19:23:09 -0700

    test we have a form

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -2,10 +2,24 @@ require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
+  end
 
+  test 'index contains a search form' do
+    get_html :index
+
+    #doc = Nokogiri::HTML(@response.body)
+    #p doc.xpath('//form').count
+    #doc.xpath('//form').each do |elem|
+    #  puts elem.inspect[0..20]
+    #  puts elem.to_xml
+    #end
+
+    form = assert_xpath('//form[ @action = "/search" ]')
+    assert_xpath form, './/input[ @name = "q" and @value = "" ]'
+    #puts form.to_xml
   end
 
   test "should get search" do
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,49 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  include Wrong
+
+  def get_html(*symbols)
+    get *symbols
+    @doc = Nokogiri::HTML(@response.body)  #  Because libxml2 + a cute wrapper does NOT suck
+  end
+
+  def assert_xpath(*args)  # assert_xpath( [container,] path ) !
+    path, container = ([@doc] + args).reverse
+    nodes = container.xpath(path)
+    node = nodes.first
+    assert node, "%s\n\tshould contain %s" % [container.to_xml, path.inspect]
+
+    #  REVIEW  If you use one assert_xpath() to isolate a small div,
+    #          and another to find a target in that div, then if
+    #          the inner assert_xpath() fails, this assertion will only
+    #          splat out that div in its error diagnostic.
+
+    #          It won't splat out the entire page, like Brand X does.
+
+    return nodes.count > 0 ? nodes : node
+  end
+
+  def deny_xpath(path)
+    assert @doc.xpath(path).empty?
+  end
+
+  def activate_partial(partial, locals = {})
+    html = render_partial(partial, locals)
+    @doc = Nokogiri::HTML(html)
+  end
+
+  def render_partial(partial, locals)
+    html = @controller.render_to_string(:partial => partial,
+                                        :layout => false,
+                                        :locals => locals)
+  end
+
+  def assert_model_updates model, field, before, after, &block
+    assert{ model.send(field) == before }
+    block.call
+    assert{ model.reload.send(field) == after }
+  end
+
 end
