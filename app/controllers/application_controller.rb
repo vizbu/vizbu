612695e7783ed52ec9d9f742c39223a9f359abe7
Amit Ambardekar <amitamb@gmail.com>
2012-06-02 03:49:10 +0530

    Added custom 404 & 500 error pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,29 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: :render_500
+    rescue_from ActionController::RoutingError, with: :render_404
+    rescue_from ActionController::UnknownController, with: :render_404
+    rescue_from ActionController::UnknownAction, with: :render_404
+    rescue_from ActiveRecord::RecordNotFound, with: :render_404
+  end
+
+  private
+
+  def render_404(exception)
+    @not_found_path = exception.message
+    respond_to do |format|
+      format.html { render template: 'errors/error_404', layout: 'layouts/application', status: 404 }
+      format.all { render nothing: true, status: 404 }
+    end
+  end
+
+  def render_500(exception)
+    @error = exception
+    respond_to do |format|
+      format.html { render template: 'errors/error_500', layout: 'layouts/application', status: 500 }
+      format.all { render nothing: true, status: 500}
+    end
+  end
 end
